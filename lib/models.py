826f4bf91b77f222b8a525bd478bc8468b4e8f8a
Ghais Zaher <ghaiszaher@gmail.com>
2020-04-10 00:19:46 +0200

    remove one resize-conv

--- lib/models.py
+++ lib/models.py
@@ -129,7 +129,7 @@ class ModelsBuilder:
                 self.upsample(512, kernel_size, norm_type=norm_type, apply_dropout=True),  # (bs, 2, 2, 1024)
                 self.upsample(512, kernel_size, norm_type=norm_type, apply_dropout=True),  # (bs, 4, 4, 1024)
                 self.upsample(512, kernel_size, norm_type=norm_type, apply_dropout=True),  # (bs, 8, 8, 1024)
-                self.resize_conv(512, kernel_size, norm_type=norm_type),  # (bs, 16, 16, 1024)
+                self.upsample(512, kernel_size, norm_type=norm_type),  # (bs, 16, 16, 1024)
                 self.resize_conv(256, kernel_size, norm_type=norm_type),  # (bs, 32, 32, 512)
                 self.resize_conv(128, kernel_size, norm_type=norm_type),  # (bs, 64, 64, 256)
                 self.resize_conv(64, kernel_size, norm_type=norm_type),  # (bs, 128, 128, 128)
