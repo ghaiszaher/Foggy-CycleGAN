8915fc94fbf15d6dbf3ba5f0934870e92d8c9c77
Ghais Zaher <ghaiszaher@gmail.com>
2020-04-14 19:09:24 +0200

    shuffle test data

--- lib/dataset.py
+++ lib/dataset.py
@@ -227,8 +227,10 @@ class DatasetInitializer:
             self.preprocess_image_path).shuffle(buffer_size)
         train_fog = tf.data.Dataset.from_generator(train_fog_gen, output_types).map(
             self.preprocess_image_path).shuffle(buffer_size)
-        test_clear = tf.data.Dataset.from_generator(test_clear_gen, output_types).map(self.preprocess_image_path)
-        test_fog = tf.data.Dataset.from_generator(test_fog_gen, output_types).map(self.preprocess_image_path)
+        test_clear = tf.data.Dataset.from_generator(test_clear_gen, output_types).map(
+            self.preprocess_image_path).shuffle(buffer_size)
+        test_fog = tf.data.Dataset.from_generator(test_fog_gen, output_types).map(
+            self.preprocess_image_path).shuffle(buffer_size)
         sample_clear = tf.data.Dataset.from_generator(sample_clear_gen, output_types).map(self.preprocess_image_path)
         sample_fog = tf.data.Dataset.from_generator(sample_fog_gen, output_types).map(self.preprocess_image_path)
 
